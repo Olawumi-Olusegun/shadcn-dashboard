74a3142d52c72d6dd998436fc02ac920c91f506e
Olawumi Olusegun <olawumi.olusegun@gmail.com>
2024-03-11 21:38:46 +0100

    Added landing page

---
+++ app/(routes)/(auth)/logout/page.tsx
@@ -0,0 +1,10 @@
+
+import React from 'react'
+
+type Props = {}
+
+export default function LogoutPage({}: Props) {
+  return (
+    <div>page</div>
+  )
+}
---
+++ app/(routes)/(pages)/(landing)/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+export default function LandingPage() {
+  return (
+    <section className="">
+      <h1>Support Me</h1>
+      <Button>Login</Button>
+      <Button>Signup</Button>
+    </section>
+  );
+}
