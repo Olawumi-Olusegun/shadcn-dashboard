52fa35bcca01f3f0f1ec30058814192d0aa9124b
Olawumi Olusegun <olawumi.olusegun@gmail.com>
2024-03-13 22:16:12 +0100

    Updated.

--- app/(routes)/(auth)/signup/page.tsx
+++ app/(routes)/(auth)/signup/page.tsx
@@ -3,7 +3,7 @@
 import React from 'react'
 import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
-import { PersonStanding } from 'lucide-react';
+import { CalendarIcon, PersonStanding } from 'lucide-react';
 import Link from 'next/link';
 import * as z from 'zod';
 import { useForm } from 'react-hook-form';
@@ -11,6 +11,8 @@ import { zodResolver } from '@hookform/resolvers/zod';
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
+import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover';
+import { Calendar } from '@/components/ui/calendar';
 
 
 const formSchema = z.object({
@@ -19,6 +21,17 @@ const formSchema = z.object({
   accountType: z.enum(["personal", "company"]),
   companyName: z.string().optional(),
   numberOfEmployees: z.coerce.number().optional(),
+  dob: z.date().refine((date) => {
+    const today = new Date();
+    const eighteenYearsAgo = new Date(
+      today.getFullYear() - 18,
+      today.getMonth(),
+      today.getDate(),
+    );
+
+    return date <= eighteenYearsAgo;
+
+  }, "You must be above eighten years")
 }).superRefine((data, context) => {
   if(data.accountType === "company" && !data.companyName) {
     context.addIssue({
@@ -50,6 +63,10 @@ export default function SignupPage() {
   });
 
   const accountType = form.watch("accountType");
+
+  const dobFromDate = new Date();
+
+  dobFromDate.setFullYear(dobFromDate.getFullYear() - 120);
 
   const handleFormSubmit = (values: any) => {
     
@@ -134,6 +151,38 @@ export default function SignupPage() {
             ) 
             : null 
             }
+
+              <FormField control={form.control} name='dob' render={({field}) => (
+              <FormItem className='flex flex-col pt-2'>
+                <FormLabel>Date of birth:</FormLabel>
+                <Popover>
+                  <PopoverTrigger asChild>
+                    <FormControl>
+                      <Button variant="outline" className='normal-case flex items-center justify-between pr-2'>
+                        <span>Pick a date</span>
+                        <CalendarIcon />
+                      </Button>
+                    </FormControl>
+                  </PopoverTrigger>
+                  <PopoverContent align='start' className='w-auto'>
+                    <Calendar
+                    mode='single'
+                    defaultMonth={field.value}
+                    selected={field.value}
+                    onSelect={field.onChange}
+                    fixedWeeks
+                    weekStartsOn={1}
+                    fromDate={dobFromDate}
+                    toDate={new Date()}
+                    className='w-[300px]'
+                    />
+                  </PopoverContent>
+                </Popover>
+                  <FormMessage />
+              </FormItem>
+              )} />
+
+
             <Button type='submit'>Sign up</Button>
 
           </form>
