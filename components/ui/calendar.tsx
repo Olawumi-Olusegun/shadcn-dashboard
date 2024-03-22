39d15a77e62f2b28d5066ddbfc23eb28adc3f1cd
Olawumi Olusegun <olawumi.olusegun@gmail.com>
2024-03-22 22:54:14 +0100

    Update

--- components/ui/calendar.tsx
+++ components/ui/calendar.tsx
@@ -75,7 +75,13 @@ function Calendar({
             const earliestYear = fromYear || fromMonth?.getFullYear() || fromDate?.getFullYear();
             const latestYear = toYear || toMonth?.getFullYear() || toDate?.getFullYear() ;
             if(earliestYear && latestYear) {
-              const yearLength = latestYear - earliestYear;
+              const yearsLength = latestYear - earliestYear;
+              selectValues = Array.from({ length: yearsLength }, (_, i) => {
+                return { 
+                  value: (earliestYear + 1).toString(),
+                  label: (earliestYear + 1).toString(),
+                 }
+              })
             }
           }
           return <Select>
